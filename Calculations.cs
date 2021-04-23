96102a558455cb34b1c09bfa513cd2b72646f768
Drowpall <drowpall@gmail.com>
2021-04-23 18:51:17 +0300

    work in progress #2
    Several new methods implemented.
    - FillList_ResNames
    - FillList_VarNames
    - WriteFile_ResNames
    - WriteFile_VarNames
    
    New field iteration_size for keeping track of Operation dependent "for" count, aswell as method
    - GetIterationSize

--- Calculations.cs
+++ Calculations.cs
@@ -20,6 +20,7 @@ namespace Course_Work_v1
         private static Operation operation;
         private static int operands_num;
         private static int digit_cap;
+        private static int iteration_size;
 
         public static void SetOperation(Operation op)
         {
@@ -58,47 +59,79 @@ namespace Course_Work_v1
             List<string> vars = new List<string>();
             List<string> res = new List<string>();
 
-            for(int i = 0; i < digit_cap * operands_num; i++)
-            {
-                vars.Add($"X{i}");
-            }
+            GetIterationSize();
 
-            //if (operation == Operation.Mult)
-            //{
-            for (int i = 0; i < 2 * digit_cap * operands_num; i++)
-            {
-                res.Add($"S{i}");
-            }
-            //}
+            FillList_VarNames(ref vars);
+            FillList_ResNames(ref res);
 
             string docPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
 
             using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "Truthtable.txt")))
             {
-                for (int i = 0; i < digit_cap * operands_num; i++)
-                {
-                    if ((i + 1) % digit_cap == 0)
-                    {
-                        outputFile.Write(vars[i] + "   ");
-                    }
-                    else
-                    {
-                        //outputFile.Write(res[i] + " ");
-                       outputFile.Write(vars[i] + "  ");
-                    }
-                }
-
-                //if (operation == Operation.Mult)
-                //{
-                for (int i = 0; i < 2 * digit_cap * operands_num; i++)
-                {
-                    outputFile.Write(res[i] + " ");
-                }
-                //}
-
+                WriteFile_VarNames(outputFile, ref vars);
+                WriteFile_ResNames(outputFile, ref res);
             }
         }
 
+        private static void FillList_ResNames(ref List<string> res)
+        {
+            for (int i = 0; i < iteration_size; i++)
+            {
+                res.Add($"S{i}");
+            }
+        }
+
+        private static void FillList_VarNames(ref List<string> vars)
+        {
+            for (int i = 0; i < digit_cap * operands_num; i++)
+            {
+                vars.Add($"X{i}");
+            }
+        }
+
+        private static void WriteFile_VarNames(StreamWriter outputFile, ref List<string> vars)
+        {
+            for (int i = 0; i < digit_cap * operands_num; i++)
+            {
+                if ((i + 1) % digit_cap == 0)
+                {
+                    outputFile.Write(vars[i] + "   ");
+                }
+                else
+                {
+                    outputFile.Write(vars[i] + "  ");
+                }
+            }
+        }
+        private static void WriteFile_ResNames(StreamWriter outputFile, ref List<string> res)
+        {
+            for (int i = 0; i < iteration_size; i++)
+            {
+                outputFile.Write(res[i] + " ");
+            }
+        }
+
+        public static void GetIterationSize()
+        {
+            switch (operation)
+            {
+                case Operation.Sum:
+                    iteration_size = digit_cap + operands_num - 1;
+                    break;
+                case Operation.Sum2:
+                    iteration_size = digit_cap;
+                    break;
+                case Operation.Mult:
+                    iteration_size = 2 * digit_cap * operands_num;
+                    break;
+                case Operation.Mult2:
+                    iteration_size = digit_cap;
+                    break;
+                default:
+                    iteration_size = -1;
+                    break;
+            }
+        }
         public static string GetOperandsNum_toString() => operands_num.ToString();
         public static string GetDigitCapacity_toString() => digit_cap.ToString();
     }
