06c81ce7e47ef244342a7c7b1409890e02ec6056
Drowpall <drowpall@gmail.com>
2021-04-25 16:05:41 +0300

    work in progress #4
    Several methods/fields implemented and refactored
    
    - rows/cols => dimension_rows/dimension_vars_cols/dimension_res_cols
    - WriteFile_X => WriteFile_TruthTable_X
    - Function calls for separate dimensions substituted for general GetDimensions() method.
    - Function calls for separate WriteFile methods substituted for general WriteFile_TruthTable_Values method.

--- Calculations.cs
+++ Calculations.cs
@@ -21,8 +21,10 @@ namespace Course_Work_v1
         private static int operands_num;
         private static int digit_cap;
         private static int iteration_size;
-        private static int rows;
-        private static int cols;
+        private static int dimension_rows;
+        private static int dimension_vars_cols;
+        private static int dimension_res_cols;
+
 
 
         public static void SetOperation(Operation op)
@@ -56,26 +58,28 @@ namespace Course_Work_v1
 
         public static void DrawTruthTable()
         {
-            GetRowsNumber();
-            GetColsNumber();
+            GetTableDimensions();
 
             List<string> vars = new List<string>();
             List<string> res = new List<string>();
-            bool[,] var_values = new bool[rows, cols];
-
-            GetIterationSize();
+            bool[,] var_values = new bool[dimension_rows, dimension_vars_cols];
+            bool[,] res_values = new bool[dimension_rows, dimension_res_cols];
 
             FillList_VarNames(ref vars);
             FillList_ResNames(ref res);
             FillMatrix_VarValues(ref var_values);
+            FillMatrix_ResValues(ref res_values);
 
             string docPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
 
             using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "Truthtable.txt")))
             {
-                WriteFile_VarNames(outputFile, ref vars);
-                WriteFile_ResNames(outputFile, ref res);
-                WriteFile_VarValues(outputFile, ref var_values);
+                WriteFile_TruthTable_VarNames(outputFile, ref vars);
+                WriteFile_TruthTable_ResNames(outputFile, ref res);
+                WriteFile_TruthTable_Values(outputFile, ref var_values, ref res_values);
+                //WriteFile_VarValues(outputFile, ref var_values);
+                //WriteFile_ResValues(outputFile, ref res_values);
+
             }
         }
 
@@ -99,17 +103,42 @@ namespace Course_Work_v1
         private static void FillMatrix_VarValues(ref bool[,] var_values)
         {
             int row_value = 0;
-            for (int i = 0; i < rows; i++)
+            for (int i = 0; i < dimension_rows; i++)
             {
-                for (int j = 0; j < cols; j++)
+                for (int j = 0; j < dimension_vars_cols; j++)
                 {
-                    var_values[i, j] = GetRightNthBit(row_value, cols - j);
+                    var_values[i, j] = GetRightNthBit(row_value, dimension_vars_cols - j);
                 }
                 row_value++;
             }
         }
-
-        private static void WriteFile_VarNames(StreamWriter outputFile, ref List<string> vars)
+        private static void FillMatrix_ResValues(ref bool[,] res_values)
+        {
+            for (int i = 0; i < dimension_rows; i++)
+            {
+                for (int j = 0; j < dimension_res_cols; j++)
+                {
+                    switch (operation)
+                    {
+                        case Operation.Sum:
+                            res_values[i, j] = false;
+                            break;
+                        case Operation.Sum2:
+                            res_values[i, j] = false;
+                            break;
+                        case Operation.Mult:
+                            res_values[i, j] = false;
+                            break;
+                        case Operation.Mult2:
+                            res_values[i, j] = false;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+        private static void WriteFile_TruthTable_VarNames(StreamWriter outputFile, ref List<string> vars)
         {
             for (int i = 0; i < digit_cap * operands_num; i++)
             {
@@ -123,7 +152,7 @@ namespace Course_Work_v1
                 }
             }
         }
-        private static void WriteFile_ResNames(StreamWriter outputFile, ref List<string> res)
+        private static void WriteFile_TruthTable_ResNames(StreamWriter outputFile, ref List<string> res)
         {
             for (int i = 0; i < iteration_size; i++)
             {
@@ -133,19 +162,55 @@ namespace Course_Work_v1
         }
         private static void WriteFile_VarValues(StreamWriter outputFile, ref bool[,] var_values)
         {
-            for (int i = 0; i < rows; i++)
+            for (int i = 0; i < dimension_rows; i++)
             {
-                for (int j = 0; j < cols; j++)
+                for (int j = 0; j < dimension_vars_cols; j++)
                 {
                     if ((j + 1) % digit_cap == 0)
                     {
                         outputFile.Write((Convert.ToInt32(var_values[i, j])).ToString() + "    ");
-                    }    
+                    }
                     else
                     {
                         outputFile.Write((Convert.ToInt32(var_values[i, j])).ToString() + "   ");
                     }
                 }
+                outputFile.WriteLine();
+            }
+        }
+        private static void WriteFile_ResValues(StreamWriter outputFile, ref bool[,] res_values)
+        {
+            for (int i = 0; i < dimension_rows; i++)
+            {
+                for (int j = 0; j < dimension_res_cols; j++)
+                {
+                    outputFile.Write((Convert.ToInt32(res_values[i, j])).ToString() + "   ");
+                }
+                outputFile.WriteLine();
+            }
+        }
+
+        private static void WriteFile_TruthTable_Values(StreamWriter outputFile, ref bool[,] var_values, ref bool[,] res_values)
+        {
+            for (int i = 0; i < dimension_rows; i++)
+            {
+                for (int j = 0; j < dimension_vars_cols; j++)
+                {
+                    if ((j + 1) % digit_cap == 0)
+                    {
+                        outputFile.Write((Convert.ToInt32(var_values[i, j])).ToString() + "    ");
+                    }
+                    else
+                    {
+                        outputFile.Write((Convert.ToInt32(var_values[i, j])).ToString() + "   ");
+                    }
+                }
+
+                for (int j = 0; j < dimension_res_cols; j++)
+                {
+                    outputFile.Write((Convert.ToInt32(res_values[i, j])).ToString() + "   ");
+                }
+
                 outputFile.WriteLine();
             }
         }
@@ -171,13 +236,24 @@ namespace Course_Work_v1
                     break;
             }
         }
-        public static void GetRowsNumber()
+        private static void GetTableDimension_Rows()
         {
-            rows = (int)Math.Pow(2, operands_num * digit_cap);
+            dimension_rows = (int)Math.Pow(2, operands_num * digit_cap);
         }
-        public static void GetColsNumber()
+        private static void GetTableDimension_ValsCols()
         {
-            cols = digit_cap * operands_num;
+            dimension_vars_cols = digit_cap * operands_num;
+        }
+        private static void GetTableDimension_ResCols()
+        {
+            dimension_res_cols = iteration_size;
+        }
+        private static void GetTableDimensions()
+        {
+            GetIterationSize();
+            GetTableDimension_Rows();
+            GetTableDimension_ValsCols();
+            GetTableDimension_ResCols();
         }
         private static bool GetRightNthBit(int val, int n)
         {
