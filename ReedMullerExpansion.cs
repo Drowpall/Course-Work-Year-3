0bc08d932ff2a3d82437680281d382a00b28de74
Drowpall <drowpall@gmail.com>
2021-05-06 19:55:12 +0300

    Add reference documentation.
    Added several features regarding algorithm source.
    
    - Added reference documentation: root/DOC/Harking_Algorightm
    - Wrote summary for calculations, given in ReedMullerExpansion class.

--- ReedMullerExpansion.cs
+++ ReedMullerExpansion.cs
@@ -9,6 +9,26 @@ using static System.Linq.Enumerable;
 
 namespace Course_Work_v1
 {
+    /// <summary>
+    /// Given algorithm implements several steps, including:
+    /// 1. Constructing binary matrix B (separate for every truth table result column):
+    ///     for given boolean function f: [f] = [f', f''],
+    ///         with [f'] = [f(0), ... ,f(2^(n-1))] and
+    ///             [f''] = [f(2^(n-1)), ... , f((2^n) - 1)]
+    ///                 then B[f] is defined as:
+    ///                             | B[f']  B[f''] |
+    ///                     B[f] =  | B[f''] B[f']  |   and B[f(i)] = f(i);
+    /// 2. Constructing binary matrix Z:
+    ///     Z^n is defined as n-th Kronecker-power of   | 1 1 |
+    ///                                                 | 0 1 |
+    /// 3. Obtaining matrix M which is calculated by the following formula:
+    ///     (B[f] * Z^n) mod 2 = M[f]
+    /// 4. For every row of matrix obtained, select one with least nonzero coefficients.
+    ///     The number of row defines optimum polarity,
+    ///         allowing us to get the shortest polynomial for given vector f.
+    ///         
+    /// * For detailed explanation and further examples, please see /DOC/Harking_Algorithm
+    /// </summary>
     internal static class ReedMullerExpansion
     {
         #region Array & Variables Declarations
